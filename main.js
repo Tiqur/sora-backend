a906f0df74f25e4a254c295fc42e63a73e0cfdce
Tiqur <Tiqur7@gmail.com>
2021-08-30 03:49:51 -0700

    Setup api router

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const apiRouter = require('./routers/apiRouter');
 
 // Initialize app and port
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/api', apiRouter);
 
 // Start server
 app.listen(port, () => console.log(`Listening on port ${port}`));
