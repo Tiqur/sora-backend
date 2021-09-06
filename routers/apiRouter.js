d7f17197138ec798f491b08722795774876b0b5c
Tiqur <Tiqur7@gmail.com>
2021-09-06 04:43:47 -0700

    Remove looping through request body

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -7,15 +7,12 @@ const Cluster = mongoose.model('Cluster', clusterSchema);
 
 router.post('/', async (req, res) => {
   if (req.body) {
-    for (const c in req.body) {
-      const b = req.body[c];
+      const b = req.body;
       const [seed, chunks, coords, repr, size] = [b.seed, b.chunks, b.coords, b.repr, b.size]
 
       console.log(repr);
       const cluster = new Cluster({seed, coords, chunks, repr, size});
       cluster.save();
-      
-    }
   }
   res.sendStatus(200);
 });
