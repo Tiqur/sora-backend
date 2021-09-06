5520cefdca7eadd1451a52402c9cf163c3ac79bd
Tiqur <Tiqur7@gmail.com>
2021-09-06 07:30:25 -0700

    Added BASIC validation

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -9,10 +9,13 @@ router.post('/', async (req, res) => {
   if (req.body) {
       const b = req.body;
       const [seed, chunks, coords, repr, size] = [b.seed, b.chunks, b.coords, b.repr, b.size]
-
-      console.log(repr);
-      const cluster = new Cluster({seed, coords, chunks, repr, size});
-      cluster.save();
+      
+      // Basic validation
+      if (seed && chunks && coords && repr && size) {
+        console.log(seed, chunks, coords, repr, size);
+        const cluster = new Cluster({seed, coords, chunks, repr, size});
+        cluster.save();
+      }
   }
   res.sendStatus(200);
 });
