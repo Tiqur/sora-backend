4e5c0eba26b74222a542d31e452437332db96ea5
Tiqur <Tiqur7@gmail.com>
2021-09-07 04:17:49 -0700

    Removed repr attr

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -8,12 +8,12 @@ const Cluster = mongoose.model('Cluster', clusterSchema);
 router.post('/', async (req, res) => {
   if (req.body) {
       const b = req.body;
-      const [seed, chunks, coords, repr, size] = [b.seed, b.chunks, b.coords, b.repr, b.size]
+      const [seed, chunks, coords, size] = [b.seed, b.chunks, b.coords, b.size]
       
       // Basic validation
-      if (seed && chunks && coords && repr && size) {
-        console.log(seed, chunks, coords, repr, size);
-        const cluster = new Cluster({seed, coords, chunks, repr, size});
+      if (seed && chunks && coords && size) {
+        console.log(seed, chunks, coords, size);
+        const cluster = new Cluster({seed, coords, chunks, size});
         cluster.save();
       }
   }
